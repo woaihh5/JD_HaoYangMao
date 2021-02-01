ff4d2b7f8af5b9b2987b55515799e09fb74741e8
JD_haoyangmao <73630068+lxdml@users.noreply.github.com>
2021-02-01 17:09:03 +0800

    Update jd_car.js

--- jd_car.js
+++ jd_car.js
@@ -43,8 +43,11 @@ if ($.isNode()) {
   cookiesArr.push(...[$.getdata('CookieJD2'), $.getdata('CookieJD')]);
   cookiesArr.reverse();
 }
-const JD_API_HOST = 'https://car-member.jd.com/api/';
+const JD_API_HOST = 'https://api.m.jd.com/';
 !(async () => {
+  $.newShareCodes = []
+  await getAuthorShareCode();
+  await getAuthorShareCode2();
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
     return;
